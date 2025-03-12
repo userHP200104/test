625fef4524cb000b99831588357d07cdfae439e6
userHP200104 <71634994+userHP200104@users.noreply.github.com>
2025-03-12 18:12:09 +0530

    test

--- api/posts.js
+++ api/posts.js
@@ -21,42 +21,52 @@ async function connectToDatabase() {
   return client;
 }
 
-module.exports = async (req, res) => {
-  const client = await connectToDatabase();
-  const db = client.db(); // Uses default database from URI; you can also specify a db name here
-  const collection = db.collection('posts');
+// module.exports = async (req, res) => {
+//   const client = await connectToDatabase();
+//   const db = client.db(); // Uses default database from URI; you can also specify a db name here
+//   const collection = db.collection('posts');
 
+//   if (req.method === 'GET') {
+//     // Read all posts
+//     const posts = await collection.find({}).toArray();
+//     res.status(200).json({ posts });
+//   } else if (req.method === 'POST') {
+//     // Create a new post
+//     const { text } = req.body;
+//     if (!text) {
+//       res.status(400).json({ error: 'Text is required' });
+//       return;
+//     }
+//     const result = await collection.insertOne({ text });
+//     res.status(201).json({ post: { _id: result.insertedId, text } });
+//   } else if (req.method === 'PUT') {
+//     // Update an existing post
+//     const { id, text } = req.body;
+//     if (!id || !text) {
+//       res.status(400).json({ error: 'ID and text are required' });
+//       return;
+//     }
+//     await collection.updateOne({ _id: new ObjectId(id) }, { $set: { text } });
+//     res.status(200).json({ message: 'Post updated' });
+//   } else if (req.method === 'DELETE') {
+//     // Delete a post
+//     const { id } = req.query;
+//     if (!id) {
+//       res.status(400).json({ error: 'ID is required' });
+//       return;
+//     }
+//     await collection.deleteOne({ _id: new ObjectId(id) });
+//     res.status(200).json({ message: 'Post deleted' });
+//   } else {
+//     res.status(405).json({ error: 'Method not allowed' });
+//   }
+// };
+
+// api/posts.js
+module.exports = async (req, res) => {
   if (req.method === 'GET') {
-    // Read all posts
-    const posts = await collection.find({}).toArray();
-    res.status(200).json({ posts });
-  } else if (req.method === 'POST') {
-    // Create a new post
-    const { text } = req.body;
-    if (!text) {
-      res.status(400).json({ error: 'Text is required' });
-      return;
-    }
-    const result = await collection.insertOne({ text });
-    res.status(201).json({ post: { _id: result.insertedId, text } });
-  } else if (req.method === 'PUT') {
-    // Update an existing post
-    const { id, text } = req.body;
-    if (!id || !text) {
-      res.status(400).json({ error: 'ID and text are required' });
-      return;
-    }
-    await collection.updateOne({ _id: new ObjectId(id) }, { $set: { text } });
-    res.status(200).json({ message: 'Post updated' });
-  } else if (req.method === 'DELETE') {
-    // Delete a post
-    const { id } = req.query;
-    if (!id) {
-      res.status(400).json({ error: 'ID is required' });
-      return;
-    }
-    await collection.deleteOne({ _id: new ObjectId(id) });
-    res.status(200).json({ message: 'Post deleted' });
+    // Return a test response to verify endpoint is working
+    res.status(200).json({ message: 'API is working!' });
   } else {
     res.status(405).json({ error: 'Method not allowed' });
   }
